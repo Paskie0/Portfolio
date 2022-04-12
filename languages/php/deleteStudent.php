6e6c71464c8cf6b332fdf5d06b0e72111457e7ca
Pascal De Wit <Pascaldewit@outlook.com>
2022-04-12 20:45:50 +0200

    small fix
    • Removed AUTO_INCREMENT reset query since it's unnecessary
    • Removed unnecessary echo statement

--- languages/php/deleteStudent.php
+++ languages/php/deleteStudent.php
@@ -4,8 +4,4 @@ include_once 'connect.php';
 $student_number = $_GET['id'];
 
 $deleteStudentInfo = "DELETE FROM students WHERE student_number = '$student_number'";
-$resetAutoIncrement = "ALTER TABLE students AUTO_INCREMENT = 1";
 $queryDeleteStudentInfo = $connect->query($deleteStudentInfo);
-$queryResetAutoIncrement = $connect->query($resetAutoIncrement);
-
-echo $_GET['id'];
