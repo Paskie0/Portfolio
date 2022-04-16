676d4401f0c8507663d8b0dc7a79ee0de83f947c
Pascal De Wit <Pascaldewit@outlook.com>
2022-04-16 11:21:54 +0200

    Accessibility Improvements
    • Replaced check mark and cross mark emoji's with plain text
    • Confirm and cancel buttons now unfocus so they don't get stuck mid-animation

--- languages/php/js/javascript.js
+++ languages/php/js/javascript.js
@@ -2,8 +2,9 @@ function toggleForm() {
   var form = document.getElementById("addStudentForm");
   if (form.style.display === "none") {
     form.style.display = "block";
-    document.getElementById("addStudentButton").innerHTML = "✔️";
-    document.getElementById("newStudentButtonCancel").style.display = "block";
+    document.getElementById("addStudentButton").innerHTML = "Confirm";
+    document.getElementById("newStudentButtonCancel").style.display = "inline";
+    document.getElementById("addStudentButton").blur();
   } else {
     form.style.display = "none";
     document.getElementById("addStudentButton").innerHTML = "Add Student";
