16008b2f2b4ac5787bffbc44e7344ab58a7800b1
Pascal De Wit <Pascaldewit@outlook.com>
2022-04-16 11:15:14 +0200

    Accessibility Improvements
    • Surrounded all form inputs by divs for bettter alignment
    • Changed tr "id" to "class" to make dom manipulation easier
    • Moved the script to the end of the body

--- languages/php/index.php
+++ languages/php/index.php
@@ -10,24 +10,36 @@ include_once 'connect.php'
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Admin Dashboard</title>
   <link rel="stylesheet" href="css/styles.css" />
-  <script src="js/javascript.js"></script>
 </head>
 
 <body>
+  <br>
   <h1>Admin Dashboard</h1>
+  <br>
   <div id="buttonsContainer">
     <button type="button" form="addStudentForm" onclick="toggleForm()" id="addStudentButton">Add Student</button>
-    <button type="button" form="addStudentForm" onclick="hideForm()" style="display: none; background-color: #21262d;" id="newStudentButtonCancel">❌</button>
+    <button type="button" form="addStudentForm" onclick="hideForm()" style="display: none; background-color: #ef5144; color:#21262d" id="newStudentButtonCancel">Cancel</button>
   </div>
   <br />
   <div id="addStudenFormContainer">
     <form action="insertStudent.php" method="POST" target="addStudentRedirect" style="display: none;" id="addStudentForm">
-      <label for="firstName">Voornaam</label><br />
-      <input type="text" id="firstName" name="firstName" /><br />
-      <label for="infix">Tussenvoegsel</label><br />
-      <input type="text" id="infix" name="infix" /><br />
-      <label for="lastName">Achternaam</label><br />
-      <input type="text" id="lastName" name="lastName" />
+      <div class="test">
+        <label for="firstName">Voornaam</label>
+        <br>
+        <input type="text" id="firstName" name="firstName" />
+      </div>
+      <br>
+      <div class="test">
+        <label for="infix">Tussenvoegsel</label>
+        <br>
+        <input type="text" id="infix" name="infix" />
+      </div>
+      <br>
+      <div class="test">
+        <label for="lastName">Achternaam</label>
+        <br>
+        <input type="text" id="lastName" name="lastName" />
+      </div>
     </form>
   </div>
   <br />
@@ -43,7 +55,7 @@ include_once 'connect.php'
     $fetchStudentInfo = "select * from students;";
     $queryFetchStudentInfo = $connect->query($fetchStudentInfo);
     while ($row = $queryFetchStudentInfo->fetch_assoc()) {
-      echo "<tr id='noBottomBorder'>";
+      echo "<tr class='noBottomBorder'>";
       echo "<td>" . $row["student_number"] . "</td>";
       echo "<td>" . $row["first_name"] . "</td>";
       echo "<td>" . $row["infix"] . "</td>";
@@ -51,12 +63,13 @@ include_once 'connect.php'
       echo "<td>";
       echo "<button id='editButton'>Edit</button>";
       echo "<form action='deleteStudent.php?id=" . $row["student_number"] . "' method='POST' target='addStudentRedirect' onclick='reload()'>";
-      echo "<button id='deleteButton' onclick=''>Delete</button>";
+      echo "<button id='deleteButton' >Delete</button>";
       echo "</form></td></tr>";
     }
     ?>
   </table>
   <iframe name="addStudentRedirect" style="display: none"></iframe>
+  <script src="js/javascript.js"></script>
 </body>
 
 </html>
