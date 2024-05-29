e5307fa17a1fdee252a5a11dda3a87b61f8ed3ef
Pascal de Wit <Pascaldewit@outlook.com>
2024-05-29 15:19:10 +0200

    Removed unused component

--- src/components/ContactToggle.js
+++ src/components/ContactToggle.js
@@ -2,18 +2,42 @@
 
 import {useHotkeys} from "react-hotkeys-hook";
 import {Button} from "@/components/ui/button";
-import ContactDrawer from "@/components/ContactDrawer";
 import {useState} from "react";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 
 export default function ContactToggle() {
   const [open, setOpen] = useState(false);
   useHotkeys("c", () => (open === false ? setOpen(true) : setOpen(false)));
 
   return (
-    <ContactDrawer open={open} onOpenChange={setOpen}>
-      <Button variant="outline" size="icon" className="h-7 w-7 border-2" onClick={() => setOpen(true)}>
-        C
-      </Button>
-    </ContactDrawer>
+    <Drawer open={open} onOpenChange={setOpen}>
+      <div className="flex items-center gap-2">
+        <span>Contact</span>
+        <DrawerTrigger asChild>
+          <Button variant="outline" size="icon" className="h-7 w-7 border-2" onClick={() => setOpen(true)}>
+            C
+          </Button>
+        </DrawerTrigger>
+      </div>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <Button>Submit</Button>
+          <DrawerClose>Close</DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 }
